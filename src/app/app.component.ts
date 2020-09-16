4c78163a311547ea8e5192283a84696a235cf10f
Christophe-R <ch.reubrecht@gmail.com>
2020-09-16 14:03:53 +0200

    add splashscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { GeolocationPosition, Plugins, CameraResultType, CameraDirection } from '@capacitor/core';
-
+import { Subscription, timer } from 'rxjs';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  @ViewChild('splashcreen', { static: true }) splashcreen: ElementRef;
+
+  subTimer: Subscription;
+  source = timer(1000, 2000);
+  constructor(private renderer: Renderer2) {}
+
   ngOnInit() {
+  this.subTimer = this.source.subscribe(val => {
+    if (val === 1) {
+      this.renderer.setStyle(this.splashcreen.nativeElement, 'display',  'none');
+      this.renderer.setStyle(this.splashcreen.nativeElement, 'visibility',  'hidden');
+    }
+  });
   }
+
+
+  ngOnDestroy(): void {
+    this.subTimer.unsubscribe();
+  }
+
 }
